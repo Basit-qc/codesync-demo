930879c469766d21ff541b2157ddfcf9277d7b86
Hafiz Muhammad Basit <basit.qc@gmail.com>
2024-05-03 16:34:28 +0500

    First commmit

---
+++ file_1.py
@@ -0,0 +1,12 @@
+"""
+First file added as file_1.py
+Let's check this file on the Web
+"""
+
+"""
+First file added as file_1.py
+Let's check this file on the Web
+"""
+
+def function_demo():
+    print(1234)
